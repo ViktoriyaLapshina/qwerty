d8b373cf875caf726399167c7c06ab2ce8e123ab
vika <psareva121197@icloud.com>
2022-09-01 15:41:33 +0400

    нпрар

---
+++ ex008/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "чипилюк")
+{
+    Console.WriteLine("Ура, это же Чипилюк!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
