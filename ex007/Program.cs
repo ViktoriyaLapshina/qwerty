a51ca3bf88ea61ebd49575256a5ed6fb5c77de1f
vika <psareva121197@icloud.com>
2022-09-01 15:10:11 +0400

    про

---
+++ ex007/Program.cs
@@ -0,0 +1,5 @@
+﻿int namberA = new Random().Next(1,10); // 1 2 3 ... 9
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1,10);
+Console.WriteLine(namberB);
+Console.WriteLine(namberA + namberB);
